713e2251b5c2ce26fb599655f596b466f642381e
Bastian <bastian@cmbt.de>
2019-11-27 16:55:26 +0100

    Routing Module aufgesetzt, erste Demo ist ein Link von content-overview auf text-content und dann auslesen der ID

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentOverviewComponent } from './content-overview/content-overview.component';
+import { TextContentComponent } from './text-content/text-content.component';
 
-const routes: Routes = [];
+/**
+ * Objekt definiert die Routen der App, bspw wird bei Eingabe von localhost:4200/overview auf
+ * den Content-Overview Komponeneten gelinkt. Redirects sind auch möglich.
+ */
+const routes: Routes = [
+  { path: 'overview', component: ContentOverviewComponent},
+  { path: '', redirectTo: 'overview', pathMatch: 'full' }, 
+  { path: 'text-content/:id', component: TextContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
