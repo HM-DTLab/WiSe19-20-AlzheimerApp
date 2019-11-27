5e4c736c3eaddf4c3638f406a93d19e7b5d1dc69
hm-firlus <firlus@hm.edu>
2019-11-27 19:33:24 +0100

    Implemented request service

---
+++ src/app/text-content-data.spec.ts
@@ -0,0 +1,7 @@
+import { TextContentData } from './text-content-data';
+
+describe('TextContentData', () => {
+  it('should create an instance', () => {
+    expect(new TextContentData()).toBeTruthy();
+  });
+});
---
+++ src/app/text-content-data.ts
@@ -0,0 +1,7 @@
+export class TextContentData {
+
+    id: number;
+    text: string;
+    hasText: boolean;
+
+}
