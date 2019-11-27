c73122327f274629d279d2ad65f52b255fef433c
hm-firlus <firlus@hm.edu>
2019-11-27 14:20:35 +0100

    Component "text-content" created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentOverviewComponent } from './content-overview/content-overview.component';
+import { TextContentButtonComponent } from './text-content-button/text-content-button.component';
+import { TextContentComponent } from './text-content/text-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentOverviewComponent
+    ContentOverviewComponent,
+    TextContentButtonComponent,
+    TextContentComponent
   ],
   imports: [
     BrowserModule,
