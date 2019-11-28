6afcad686e7d47a0984f544cc268fd33368e35e1
Bastian <bastian@cmbt.de>
2019-11-28 08:21:18 +0100

    Demoanfrage an data-service
    Für die Id wird der status hasText ausgelesen

--- src/app/text-content/text-content.component.ts
+++ src/app/text-content/text-content.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { DataServiceService } from '../data-service.service';
+import { TextContentData } from '../text-content-data';
 
 @Component({
   selector: 'app-text-content',
@@ -9,13 +11,16 @@ import { Location } from '@angular/common';
 })
 export class TextContentComponent implements OnInit {
 
+  // Boolean Variable, soll Wert von data-service enthalten, wird in contactDataService benutzt
+  private hasText : boolean;
   // Jede Komponente die die Location, also die aktuelle Position Nutzen möchte muss folgendes importieren:
   // import { Location } from '@angular/common';
   // zusätzlich muss im Konstruktor das location Objekt initialisiert werden.
   // Für das tracken der ID in der URL wird ein ActivatedRoute Objekt verwendet.
   constructor(
     private activatedRoute : ActivatedRoute,
-    private location : Location
+    private location : Location,
+    private dataService : DataServiceService
   ) { }
 
   ngOnInit() {
@@ -29,7 +34,19 @@ export class TextContentComponent implements OnInit {
   getId() {
     const id = +this.activatedRoute.snapshot.paramMap.get('id');
     console.log(id);
+    this.contactDataService(id);
     return  Number.isNaN(id)? null : id;
+  }
+
+  // Kontaktiert den DataService und holt sich für die entsprechende Id den Status hasText und speichert
+  // ihn in die globale Variable hasText
+  contactDataService(id: number) {
+    this.dataService.getTextContent(id)
+    .subscribe(res => {
+      this.hasText = res.hasText
+      console.log(this.hasText);
+    });
+    
   }
 
   // Nach initialisieren des Location Objektes kann es bspw. für einen back Button genutzt werden 
